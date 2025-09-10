68f46cc5df35c189d0e5d165e32d2f0db7f5c8c4
petrussan <petrus@petra.ac.id>
2025-09-10 10:28:14 +0700

    Delete lecture 04/com/example/universitas directory

--- lecture 04/com/example/universitas/testKomposisi.java
+++
@@ -1,9 +0,0 @@
-package com.example.universitas;
-
-public class testKomposisi {
-    public static void main(String[] args) {
-        Gedung gedungP = new Gedung("P", 20);
-        gedungP.displayRuang();
-    }
-    
-} 
